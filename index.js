ae1f87f78b46b39db6872c8205c53e40b60ccaca
Vinicius Gabriel <viniciusrichard98@gmail.com>
2023-07-25 15:07:26 -0300

    finalizando projeto

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let botao = document.getElementById('botao-nav');
 let botaoNav = document.getElementById('botao-nav');
 let navM = document.getElementById('nav-mob');
+let sombra = document.getElementById('sombra');
+
+console.log(sombra);
 
 function abrir() {
     if (navM.style.visibility == "visible") {
         navM.style.visibility = "hidden";
+        sombra.style.visibility = "hidden";
     } else {
         navM.style.visibility = "visible";
+        sombra.style.visibility = "visible";
+
     }
 }
